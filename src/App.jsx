2f8df78ebfd2d688cf3f4d2af9e581da3fb0da45
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-08-25 09:21:18 +0200

    SOLVE RESPONSIVE

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import Countdown from './components/Countdown/Countdown'
 import DigitalClock from './components/DigitalClock/DigitalClock'
 import Stopwatch from './components/Stopwatch/Stopwatch'
+import Footer from './components/Footer/Footer'
+
+
 
 
 function App() {
@@ -13,6 +16,7 @@ function App() {
       <DigitalClock/>
       <Countdown/>
       <Stopwatch/>
+      <Footer/>
       
     </div>
   )
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+    
+    <footer>
+
+        
+        <div>
+            <ul>
+                <li>@Pedro Lerida Nieto 2022</li>
+                <li className="image_footer"><a href="https://github.com/pedroleni" target="_blank"><img src="https://avatars.githubusercontent.com/u/38913098?v=4" alt="github"/></a></li>
+                <li className="image_footer"><a href="https://www.linkedin.com/in/pedro-l%C3%A9rida-nieto-260501229/" target="_blank"><img src="https://media.discordapp.net/attachments/701164137081733201/1008557611647500348/linkedin_copia.png" alt="linkedin"/></a></li>
+            </ul>
+       
+        </div>
+    </footer>
+    </>
+    
+  );
+};
+
+export default Footer;
